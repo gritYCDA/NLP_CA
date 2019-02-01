36ae13f2def5a488694cc0ccd73bb9b7643587f0
genie97 <qndnd1@naver.com>
2019-02-01 14:06:23 +0900

    init project

--- Project_PS/Project_PS/marketingSite/main/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserInfo(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
